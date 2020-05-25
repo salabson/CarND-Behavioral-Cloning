86c0fb581d80770af8feb498475a0214c927af40
=Salabson <salabson4@yahoo.co.uk>
2020-05-25 09:46:09 +0000

    Implement LeNet Network

--- model.py
+++ model.py
@@ -18,6 +18,9 @@ with open('/home/workspace/CarND-Behavioral-Cloning-P3/data/driving_log.csv') as
         image = cv2.imread(current_path)
         images.append(image)
         measurements.append(float(line[3]))
+        
+
+
     
 print(images[0].shape)
 X_train = np.array(images)
@@ -29,15 +32,20 @@ print("Number of training data {}".format(len(X_train)))
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Flatten, Lambda
-from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x/255-0.5, input_shape=(160,320,3)))
-model.add(
+model.add(Conv2D(6, (5,5), activation='relu'))
+model.add(MaxPooling2D((2,2)))
+model.add(Conv2D(16, (5,5), activation='relu'))
+model.add(MaxPooling2D((2,2)))               
 model.add(Flatten())
+model.add(Dense(120, activation='relu'))   
+model.add(Dense(84, activation='relu'))
 model.add(Dense(1))
 
 model.compile(optimizer="adam", loss="mse")
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=1)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
 model.save("model.h5")
